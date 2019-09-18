9610cf904980e8fa37df8528d6556055691cc1e1
galiii <gali.brill.1408@gmail.com>
2019-09-18 18:29:59 +0300

    first commitcd

--- my-app/src/components/Todo.js
+++ my-app/src/components/Todo.js
@@ -6,31 +6,27 @@ class Todo extends React.Component{
 
 constructor(props){
  super(props);
- 
  this.state = {
  tasks : [], //to store every value witch we passing to our todo list
  term :'' //to store what we passing as a value to our input 
  };
-
 }
 
 
-//input on value onAddItem method
+/*
+input on value onAddItem method
+value - target takes what stored in our state as term
+onAddItem is a function witch changes our state depanding on the current input value.
+to change state we need this.setState() witch also triggers our UI updates
+*/
 onAddItem = (e) => {
-  /*
-  value - target takes what stored in our state as term
-
-  onAddItem is a function witch changes our state depanding on the current input value.
-  to change state we need this.setState() witch also triggers our UI updates
-  */
   this.setState({term: e.target.value});
 }
 
 //Form with onSubmit method 
 //Now we need to warp our input field with an add button
 onSubmitItem = (e) => {
-  //preventDefualt() method stops default action of an element from happing 
-  e.preventDefault(); 
+  e.preventDefault(); //method stops default action of an element from happing  
 
   this.setState({
   /*
@@ -51,9 +47,9 @@ deleteItem = (taskDelete,index) => {
 const {tasks} = this.state; 
  console.log(taskDelete);
  console.log(index);
- //Using slice
- tasks.splice(index, 1);
- this.setState({ tasks:tasks});
+ 
+ tasks.splice(index, 1);//Using slice
+ this.setState({tasks:tasks});
 }
 
 render(){
@@ -64,21 +60,16 @@ render(){
             tasks={this.state.tasks}
             deleteItem={this.deleteItem}
             />
-          <form  onSubmit={this.onSubmitItem}>
-             
+          <form  onSubmit={this.onSubmitItem}>          
             <input 
               value={this.state.term} 
               onChange ={this.onAddItem} 
               placeholder="Add Task">
             </input> 
             <button>ADD</button>
-          </form>
-          
+          </form>    
       </div>
   );  
-}
-
-};
-
+}};
 
 export default Todo;
