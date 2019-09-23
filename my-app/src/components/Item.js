70aa6241f9db3e650d1df688eac20544318b77f8
galiii <gali.brill.1408@gmail.com>
2019-09-23 12:28:51 +0300

    the position of every element is on the right position

--- my-app/src/components/Item.js
+++ my-app/src/components/Item.js
@@ -3,18 +3,22 @@ import React from "react";
 export default function Item({ index, done, title, dueDate, stat, usr }) {
   return (
     <li>
-      <div className="item-list">
-        <span
-          className={
-            done
-              ? "fas fa-check done-item check-item"
-              : "fas fa-check undone-item check-item"
-          }
-        ></span>
-        <span className="item-title">{title}</span>
-        <span>{dueDate}</span>
-        <span className="status-item">{stat}</span>
-        <span className="img-usr">{usr}</span>
+      <div className="warpper-list">
+        <div className="start-warp">
+          <div
+            className={
+              done
+                ? "fas fa-check done-item check-item"
+                : "fas fa-check undone-item check-item"
+            }
+          ></div>
+          <div className="item-title">{title}</div>
+        </div>
+        <div className="end-warp">
+          <div className="date-item">{dueDate}</div>
+          <div className="status-item">{stat}</div>
+          <div className="img-usr">{usr}</div>
+        </div>
       </div>
     </li>
   );
