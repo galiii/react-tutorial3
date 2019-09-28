5e4e9ed508c5e33f7d7aed293cfc981661bdc601
galiii <gali.brill.1408@gmail.com>
2019-09-28 18:50:16 +0300

    adding a form starting to understand the input and so on

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,15 @@
-import React from 'react';
+import React from "react";
 //import logo from './logo.svg';
-import './App.css';
-import Todo from './components/Todo';
+import "./App.css";
+//import Todo from "./components/Todo";
+import FormTask from "./components/FormTask";
 
 function App() {
   return (
     <div className="App">
       <div className=" App-header">
-        <Todo />
+        <FormTask />
       </div>
-      
     </div>
   );
 }
---
+++ my-app/src/components/FormTask.js
@@ -0,0 +1,66 @@
+import React from "react";
+
+class FormTask extends React.Component {
+  state = {
+    title: "New Task",
+    done: false,
+    stats: "",
+    dueDate: new Date().toLocaleString()
+  };
+
+  handleTitleChange = event => {
+    this.setState({
+      title: event.target.value
+    });
+    console.log("The title is ", this.state.title);
+  };
+
+  handleStatusChange = event => {
+    this.setState({
+      stats: event.target.value
+    });
+    console.log("The stats is ", this.state.stats);
+  };
+
+  render() {
+    return (
+      <div className="add-task">
+        <form>
+          <div>Add New Task</div>
+          <label htmlFor="title">
+            Add title:
+            <input
+              id="title"
+              value={this.state.title}
+              onChange={this.handleTitleChange}
+            />
+          </label>
+
+          <label htmlFor="date">
+            Date {this.state.dueDate}
+            <input id="dueDate" type="date" step="7"></input>
+          </label>
+          <label htmlFor="stats">
+            Choose status:
+            <select
+              id="stats"
+              value={this.state.stats}
+              onChange={this.handleStatusChange}
+              onBlur={this.handleStatusChange}
+            >
+              <option>All</option>
+
+              {["in Beta", "planning", "a/b test"].map(item => (
+                <option key={item} value={item}>
+                  {item}
+                </option>
+              ))}
+            </select>
+          </label>
+        </form>
+      </div>
+    );
+  }
+}
+
+export default FormTask;
