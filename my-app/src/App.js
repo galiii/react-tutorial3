6495f6be4386883ade0073026b27c2bc1d598fd8
galiii <gali.brill.1408@gmail.com>
2019-10-02 18:06:55 +0300

    some style

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,11 +1,15 @@
 import React from "react";
 //import logo from './logo.svg';
+import mockup from "./mockup.png";
 import "./App.css";
 import Todo from "./components/Todo";
 
 function App() {
   return (
     <div className="App">
+      <div className="mockup">
+        <img src={mockup} />
+      </div>
       <div className="App-header">
         <Todo />
       </div>
